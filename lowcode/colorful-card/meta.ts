efcb37230070236c788b69fafc5cd7738671dfd7
tower1229 <tower1229@foxmail.com>
2022-10-07 17:36:07 +0800

    format

--- lowcode/colorful-card/meta.ts
+++ lowcode/colorful-card/meta.ts
@@ -1,112 +1,112 @@
-import { ComponentMetadata, Snippet } from "@alilc/lowcode-types";
-import { uuid } from "../util";
+import { ComponentMetadata, Snippet } from '@alilc/lowcode-types';
+import { uuid } from '../util';
 
 const ColorfulCardMeta: ComponentMetadata = {
-  componentName: "ColorfulCard",
-  title: "ColorfulCard",
-  docUrl: "",
-  screenshot: "",
-  devMode: "proCode",
+  componentName: 'ColorfulCard',
+  title: 'ColorfulCard',
+  docUrl: '',
+  screenshot: '',
+  devMode: 'proCode',
   npm: {
-    package: "zhizhou-material-relay",
-    version: "0.0.26",
-    exportName: "ColorfulCard",
-    main: "src/index.tsx",
+    package: 'zhizhou-material-relay',
+    version: '0.0.26',
+    exportName: 'ColorfulCard',
+    main: 'src/index.tsx',
     destructuring: true,
-    subName: "",
+    subName: '',
   },
   props: [
     {
-      name: "styleTyle",
-      title: { label: "样式" },
+      name: 'styleTyle',
+      title: { label: '样式' },
       setter: [
         {
-          componentName: "RadioGroupSetter",
+          componentName: 'RadioGroupSetter',
           props: {
             options: [
               {
-                title: "卡片",
-                value: "normal",
+                title: '卡片',
+                value: 'normal',
               },
               {
-                title: "透明",
-                value: "transparent",
+                title: '透明',
+                value: 'transparent',
               },
             ],
           },
         },
       ],
-      defaultValue: "normal",
+      defaultValue: 'normal',
       isRequired: true,
     },
     {
-      name: "title",
-      title: { label: "标题" },
-      propType: { type: "oneOfType", value: ["string"] },
-      defaultValue: "分组标题",
+      name: 'title',
+      title: { label: '标题' },
+      propType: { type: 'oneOfType', value: ['string'] },
+      defaultValue: '分组标题',
     },
     {
-      name: "subTitle",
-      title: { label: "子标题" },
-      propType: { type: "oneOfType", value: ["string"] },
+      name: 'subTitle',
+      title: { label: '子标题' },
+      propType: { type: 'oneOfType', value: ['string'] },
     },
     {
-      name: "description",
-      title: { label: "解释文案" },
-      propType: { type: "oneOfType", value: ["string"] },
+      name: 'description',
+      title: { label: '解释文案' },
+      propType: { type: 'oneOfType', value: ['string'] },
     },
     {
-      name: "withBorder",
-      title: { label: "头部分割线" },
-      propType: "bool",
+      name: 'withBorder',
+      title: { label: '头部分割线' },
+      propType: 'bool',
       defaultValue: true,
-      setter: "BoolSetter",
+      setter: 'BoolSetter',
       supportVariable: true,
     },
     {
-      name: "withCollapse",
-      title: { label: "收起操作" },
-      propType: "bool",
+      name: 'withCollapse',
+      title: { label: '收起操作' },
+      propType: 'bool',
       defaultValue: true,
-      setter: "BoolSetter",
+      setter: 'BoolSetter',
       supportVariable: true,
     },
     {
-      name: "withPadding",
-      title: { label: "内容区间距" },
-      propType: "bool",
+      name: 'withPadding',
+      title: { label: '内容区间距' },
+      propType: 'bool',
       defaultValue: true,
-      setter: "BoolSetter",
+      setter: 'BoolSetter',
       supportVariable: true,
     },
     {
-      name: "headerExt",
-      title: { label: "操作项" },
+      name: 'headerExt',
+      title: { label: '操作项' },
       setter: {
-        componentName: "ArraySetter",
+        componentName: 'ArraySetter',
         props: {
           itemSetter: {
-            componentName: "ObjectSetter",
+            componentName: 'ObjectSetter',
             props: {
               config: {
                 items: [
                   {
-                    name: "key",
-                    title: "key",
-                    setter: "StringSetter",
+                    name: 'key',
+                    title: 'key',
+                    setter: 'StringSetter',
                     initialValue: (val) => val || uuid(),
                     supportVariable: true,
                   },
                   {
-                    name: "text",
-                    title: "按钮文字",
-                    setter: "StringSetter",
-                    initialValue: "操作项",
+                    name: 'text',
+                    title: '按钮文字',
+                    setter: 'StringSetter',
+                    initialValue: '操作项',
                   },
                   {
-                    name: "onClick",
-                    title: "点击事件",
-                    setter: "FunctionSetter",
+                    name: 'onClick',
+                    title: '点击事件',
+                    setter: 'FunctionSetter',
                   },
                 ],
               },
@@ -114,7 +114,7 @@ const ColorfulCardMeta: ComponentMetadata = {
             initialValue: () => {
               return {
                 key: uuid(),
-                text: "操作项",
+                text: '操作项',
               };
             },
           },
@@ -128,21 +128,15 @@ const ColorfulCardMeta: ComponentMetadata = {
     },
     supports: {
       style: true,
-      events: [
-        {
-          name: "onClick",
-          template: "onClick(key,${extParams}){\n// 操作项回调\nconsole.log("onClick", key);}",
-        },
-      ],
     },
   },
 };
 const snippets: Snippet[] = [
   {
-    title: "分组",
-    screenshot: "",
+    title: '分组',
+    screenshot: '',
     schema: {
-      componentName: "ColorfulCard",
+      componentName: 'ColorfulCard',
       props: {},
     },
   },
