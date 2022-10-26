0d1646ff4c8303755c52275d02fd8b8f1e199bd2
tower1229 <tower1229@foxmail.com>
2022-10-26 21:09:07 +0800

    feat: add linkbox

--- lowcode/colorful-link-box/meta.ts
+++ lowcode/colorful-link-box/meta.ts
@@ -8,7 +8,7 @@ const ColorfulLinkBoxMeta: ComponentMetadata = {
   devMode: 'proCode',
   npm: {
     package: 'zhizhou-material-relay',
-    version: '0.0.26',
+    version: '0.1.30',
     exportName: 'ColorfulLinkBox',
     main: 'src/index.tsx',
     destructuring: true,
@@ -16,64 +16,115 @@ const ColorfulLinkBoxMeta: ComponentMetadata = {
   },
   props: [
     {
-      name: 'alias',
-      title: { label: '别名' },
-      propType: { type: 'oneOfType', value: ['string', 'node'] },
-    },
-    {
-      name: 'url',
-      title: { label: 'iframe地址' },
-      propType: { type: 'oneOfType', value: ['string'] },
-    },
-    {
-      name: 'autoHeight',
-      title: { label: '高度自适应' },
-      propType: 'bool',
-      defaultValue: false,
-      setter: 'BoolSetter',
-      supportVariable: true,
-    },
-    {
-      name: 'offset',
-      title: { label: '尺寸' },
-      propType: 'object',
+      name: 'type',
+      title: { label: '' },
+      propType: { type: 'oneOf', value: ['inside', 'outside'] },
+      defaultValue: 'inside',
       setter: {
-        componentName: 'ObjectSetter',
+        componentName: 'RadioGroupSetter',
         props: {
-          config: {
-            items: [
-              {
-                name: 'height',
-                title: { label: '内容区高度', tip: '内容区高度' },
-                propType: { type: 'oneOfType', value: ['string', 'number'] },
-                setter: [
+          options: [
+            {
+              title: '内部链接',
+              value: 'inside',
+            },
+            {
+              title: '外部链接',
+              value: 'outside',
+            },
+          ],
+        },
+      },
+    },
+    {
+      name: 'route',
+      title: { label: '链接地址' },
+      propType: 'string',
+      setter: ['StringSetter'],
+      condition: {
+        type: 'JSFunction',
+        value:
+          "condition(target) {\n          return target.getProps().getPropValue('type') === 'inside';\n        }",
+      },
+    },
+    {
+      name: 'params',
+      title: {
+        label: '携带参数',
+      },
+      propType: 'string',
+      setter: {
+        componentName: 'ArraySetter',
+        props: {
+          itemSetter: {
+            componentName: 'ObjectSetter',
+            props: {
+              config: {
+                items: [
                   {
-                    componentName: 'NumberSetter',
-                    initialValue: 500,
+                    name: 'key',
+                    title: 'key',
+                    setter: 'StringSetter',
+                    supportVariable: true,
+                  },
+                  {
+                    name: 'value',
+                    title: 'value',
+                    setter: 'StringSetter',
+                    supportVariable: true,
                   },
                 ],
-                isRequired: false,
               },
-              {
-                name: 'marginTop',
-                title: { label: '顶部空间', tip: '顶部空间' },
-                propType: { type: 'oneOfType', value: ['string', 'number'] },
-                setter: [
-                  {
-                    componentName: 'NumberSetter',
-                    initialValue: 0,
-                  },
-                ],
-                isRequired: false,
-              },
-            ],
+            },
           },
         },
+      },
+      condition: {
+        type: 'JSFunction',
+        value:
+          "condition(target) {\n          return target.getProps().getPropValue('type') === 'inside';\n        }",
+      },
+    },
+    {
+      name: 'href',
+      title: { label: '跳转链接', tip: '跳转链接' },
+      propType: 'string',
+      setter: 'StringSetter',
+      defaultValue: '',
+      condition: {
+        type: 'JSFunction',
+        value:
+          "condition(target) {\n          return target.getProps().getPropValue('type') === 'outside';\n        }",
+      },
+    },
+    {
+      name: 'target',
+      title: { label: '跳转位置', tip: '在何处显示链接的资源' },
+      propType: {
+        type: 'oneOf',
+        value: ['_self', '_blank', '_parent', '_top'],
+      },
+      defaultValue: '_self',
+      condition: {
+        type: 'JSFunction',
+        value:
+          "condition(target) {\n          return target.getProps().getPropValue('type') === 'outside';\n        }",
+      },
+    },
+    {
+      name: 'clickHandler',
+      title: { label: '点击事件' },
+      setter: 'FunctionSetter',
+      extraProps: {
+        display: 'block',
       },
     },
   ],
   configure: {
-    component: {},
+    supports: { style: true },
+    component: {
+      isContainer: true,
+    },
   },
 };
 const snippets: Snippet[] = [
--- src/components/colorful-link-box/index.tsx
+++ src/components/colorful-link-box/index.tsx
@@ -1,11 +1,40 @@
-import * as React from 'react';
+import React from 'react';
 import { createElement } from 'react';
 import './index.scss';
 
 const ColorfulLinkBox: any = (props: any) => {
   console.warn('ColorfulLinkBox=', props);
 
-  return <div>{JSON.stringify(props)}</div>;
+  function handleClick(e) {
+    // 自定义事件
+    if (Array.isArray(props.clickHandler)) {
+      props.clickHandler.forEach((handler) => {
+        Array.isArray(handler.actionList) &&
+          handler.actionList.forEach((action) => {
+            console.log('ColorfulLinkBox 自定义事件：', action);
+          });
+      });
+    }
+    if (props.type === 'inside') {
+      // 内部链接
+      e.preventDefault();
+
+      if (props.route) {
+        let routeAddress = `${props.route}`;
+
+        if (Array.isArray(props.params)) {
+          routeAddress += `?${props.params.map((p) => `${p.key}=${p.value}`).join('&')}`;
+        }
+
+        console.warn('routeAddress=', routeAddress);
+        window.open(routeAddress);
+      } else {
+        console.warn('ColorfulLinkBox route error:', props.route, props.params);
+      }
+    }
+  }
+
+  return <div onClick={handleClick}>{props.children}</div>;
 };
 
 export default ColorfulLinkBox;
