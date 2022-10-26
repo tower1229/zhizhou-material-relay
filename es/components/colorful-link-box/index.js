abbca3e648b17a33b1f10f244cab4a89d688073e
tower1229 <tower1229@foxmail.com>
2022-10-26 21:11:32 +0800

    0.1.30

--- es/components/colorful-link-box/index.js
+++ es/components/colorful-link-box/index.js
@@ -1,9 +1,43 @@
-import * as React from 'react';
+import React from 'react';
 import "./index.scss";
 
 var ColorfulLinkBox = function ColorfulLinkBox(props) {
   console.warn('ColorfulLinkBox=', props);
-  return /*#__PURE__*/React.createElement("div", null, JSON.stringify(props));
+
+  function handleClick(e) {
+    // 自定义事件
+    if (Array.isArray(props.clickHandler)) {
+      props.clickHandler.forEach(function (handler) {
+        Array.isArray(handler.actionList) && handler.actionList.forEach(function (action) {
+          console.log('ColorfulLinkBox 自定义事件：', action);
+        });
+      });
+    }
+
+    if (props.type === 'inside') {
+      // 内部链接
+      e.preventDefault();
+
+      if (props.route) {
+        var routeAddress = "" + props.route;
+
+        if (Array.isArray(props.params)) {
+          routeAddress += "?" + props.params.map(function (p) {
+            return p.key + "=" + p.value;
+          }).join('&');
+        }
+
+        console.warn('routeAddress=', routeAddress);
+        window.open(routeAddress);
+      } else {
+        console.warn('ColorfulLinkBox route error:', props.route, props.params);
+      }
+    }
+  }
+
+  return /*#__PURE__*/React.createElement("div", {
+    onClick: handleClick
+  }, props.children);
 };
 
 export default ColorfulLinkBox;
